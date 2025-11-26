70fed5bc26ee1b8c1ba2255b30986533a22c9160
William-Daniel-Maistry <166234162+William-Daniel-Maistry@users.noreply.github.com>
2025-11-27 00:09:08 +0200

    Updated Gallery

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,53 +1,96 @@
 import { sliderAutoPlusNavigation } from "./utils/sliderAutoPlusNavigation.js";
 
-const communityFeedingImages = [
-  "feeding-1.jpg",
-  "feeding-2.jpg",
-  "feeding-3.jpg",
-  "feeding-4.jpg",
-  "feeding-5.jpg",
-  "feeding-6.jpg",
-  "feeding-7.jpg",
-  "feeding-8.jpg",
-  "feeding-9.jpg",
-  "feeding-10.jpg",
-  "feeding-11.jpg",
-  "feeding-12.jpg",
-  "feeding-13.jpg",
-  "feeding-14.jpg",
-  "feeding-15.jpg",
-  "feeding-16.jpg",
-  "feeding-17.jpg",
-  "feeding-18.jpg",
-  "feeding-19.jpg",
-  "feeding-20.jpg",
-  "feeding-21.jpg",
-  "feeding-22.jpg",
-  "feeding-23.jpg",
-  "feeding-24.jpg",
-  "feeding-25.jpg",
-  "feeding-26.jpg",
-  "feeding-27.jpg",
-  "feeding-28.jpg",
-  "feeding-29.jpg",
-  "feeding-30.jpg",
-  "feeding-31.jpg",
-  "feeding-32.jpg",
-  "feeding-33.jpg",
-  "feeding-34.jpg",
-];
+
+const communityEventsImages = [
+  "1.jpg",
+  "2.jpg",
+  "3.jpg",
+  "4.jpg",
+  "5.jpg",
+  "6.jpg",
+  "7.jpg",
+  "8.jpg",
+  "9.jpg",
+  "10.jpg",
+  "11.jpg",
+  "12.jpg",
+  "13.jpg",
+  "14.jpg",
+  "15.jpg",
+  "16.jpg",
+  "17.jpg",
+  "18.jpg",
+  "19.jpg",
+  "20.jpg",
+  "21.jpg",
+  "22.jpg",
+  "23.jpg",
+  "24.jpg",
+  "25.jpg",
+  "26.jpg",
+  "27.jpg",
+  "28.jpg",
+  "29.jpg",
+  "30.jpg",
+  "31.jpg",
+  "32.jpg",
+  "33.jpg",
+  "34.jpg",
+  "35.jpg",
+  "36.jpg",
+  "37.jpg",
+  "38.jpg",
+  "39.jpg",
+  "40.jpg",
+  "41.jpg",
+  "42.jpg",
+  "43.jpg",
+  "44.jpg",
+  "45.jpg",
+  "46.jpg",
+  "47.jpg",
+  "48.jpg",
+  "49.jpg",
+  "50.jpg",
+  "51.jpg",
+  "52.jpg",
+  "53.jpg",
+  "54.jpg",
+  "55.jpg",
+  "56.jpg",
+  "57.jpg",
+  "58.jpg",
+  "59.jpg",
+  "60.jpg",
+  "61.jpg",
+  "62.jpg",
+  "63.jpg",
+  "64.jpg",
+  "65.jpg",
+  "66.jpg",
+  "67.jpg",
+  "68.jpg",
+  "69.jpg",
+  "70.jpg",
+  "71.jpg",
+  "72.jpg",
+  "73.jpg",
+  "74.jpg",
+  "75.jpg",
+  "76.jpg"
+]
 
 
 sliderAutoPlusNavigation(
-  "community-feeding__slider", 
-  communityFeedingImages, 
+  "community-events__slider", 
+  communityEventsImages, 
   "navPrev", 
   "play", 
   "pause", 
   "navNext", 
-  "community-feeding__slider--pagination", 
+  "community-events__slider--pagination", 
   {
-    imagesRootPath: "images/feeding/",
+    imagesRootPath: "images/gallery/",
     activePaginationImgStyles: "5px solid gold"
   });
 
@@ -64,14 +107,11 @@ openVid(3);
 openVid(4);
 openVid(5);
 openVid(6);
-openVid(7);
-openVid(8);
-openVid(9);
 
 
 
 function setVideoContainerHeight() {
-  const vidHeight = document.querySelector("video").getBoundingClientRect().height
+  const vidHeight = document.querySelector(".video-container img").getBoundingClientRect().height
 
   document.querySelectorAll(".video-container").forEach(video => {
     video.style.height = `${vidHeight}px`;
